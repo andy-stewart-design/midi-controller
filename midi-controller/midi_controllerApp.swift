e240f879001751fc00fb1c85b8792050782c6d14
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-01-26 15:10:42 -0500

    Initial Commit

---
+++ midi-controller/midi_controllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  midi_controllerApp.swift
+//  midi-controller
+//
+//  Created by Andy Stewart on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct midi_controllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
